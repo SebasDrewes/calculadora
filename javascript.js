cb8913f64dbdcb80b766f3b8db78c328edd8970d
Sebastian Drewes <sebas.drewes@gmail.com>
2021-05-08 22:54:53 -0300

    fixed 0

--- javascript.js
+++ javascript.js
@@ -148,3 +148,45 @@ backSpace.addEventListener('click', () => {
     display.textContent = display.textContent.slice(0, -1);
     punto.disabled = false;
 });
+
+
+window.addEventListener("keydown", function (event) {
+    if (event.defaultPrevented) {
+      return; // Do nothing if the event was already processed
+    }
+  
+    switch (event.key) {
+      case "1": // IE/Edge specific value
+        display.textContent  = display.textContent  + this.innerHTML;
+        if (operador === "") {
+            primerNumero = primerNumero + this.innerHTML; 
+        } else {
+            segundoNumero = segundoNumero + this.innerHTML;
+        }
+        break;
+      case "Up": // IE/Edge specific value
+      case "ArrowUp":
+        // Do something for "up arrow" key press.
+        break;
+      case "Left": // IE/Edge specific value
+      case "ArrowLeft":
+        // Do something for "left arrow" key press.
+        break;
+      case "Right": // IE/Edge specific value
+      case "ArrowRight":
+        // Do something for "right arrow" key press.
+        break;
+      case "Enter":
+        // Do something for "enter" or "return" key press.
+        break;
+      case "Esc": // IE/Edge specific value
+      case "Escape":
+        // Do something for "esc" key press.
+        break;
+      default:
+        return; // Quit when this doesn't handle the key event.
+    }
+  
+    // Cancel the default action to avoid it being handled twice
+    event.preventDefault();
+  }, true);
